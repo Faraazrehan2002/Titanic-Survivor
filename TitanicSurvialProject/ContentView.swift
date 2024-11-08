c62528d6b9e62913634e012b65934a211fac00b9
Faraazrehan2002 <faraazrehan2002@gmail.com>
2024-11-07 20:56:18 -0600

    Initial Commit

---
+++ TitanicSurvialProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TitanicSurvialProject
+//
+//  Created by Faraaz Rehan Junaidi Mohammed on 11/7/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TitanicSurvialProject/TitanicSurvialProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TitanicSurvialProjectApp.swift
+//  TitanicSurvialProject
+//
+//  Created by Faraaz Rehan Junaidi Mohammed on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TitanicSurvialProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
